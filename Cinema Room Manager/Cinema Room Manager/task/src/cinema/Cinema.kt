27558bb1b61118cf1eaf4b47895ab5859e648adf
VicN <vnikolaeff989@gmail.com>
2022-10-08 21:38:04 +0300

    Project: Cinema Room Manager. Stage 3/5 complete

--- Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -7,10 +7,38 @@ fun main() {
     val seats = readln().toInt()
 
     val allSeats = rows * seats
-    val income =
-            if (allSeats <= 60)
-                allSeats * 10
-            else
-                (rows / 2 * seats * 10) + (rows - rows / 2) * seats * 8
-    println("Total income:\n$$income")
+
+    val cinema = MutableList(rows) { MutableList(seats) { "S" } }
+    printCinema(cinema)
+
+    println("Enter a row number:")
+    val row = readln().toInt()
+    println("Enter a seat number in that row:")
+    val seat = readln().toInt()
+
+    val price =
+        if (allSeats <= 60)
+            10
+        else
+            if (row <= rows / 2) 10 else 8
+
+    println("\nTicket price: $$price")
+
+    cinema[row - 1][seat - 1] = "B"
+    printCinema(cinema)
+}
+
+fun printCinema(cinema: List<List<Any>>) {
+    print("\nCinema:\n  ")
+    repeat(cinema[0].size) { print("${it + 1} ") }
+    println()
+
+    for (i in cinema.indices) {
+        print("${i + 1} ")
+        for (j in cinema[i]) {
+            print("$j ")
+        }
+        println()
+    }
+    println()
 }
