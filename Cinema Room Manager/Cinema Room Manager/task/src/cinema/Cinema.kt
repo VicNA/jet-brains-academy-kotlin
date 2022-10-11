4b0e807d67d15907714cf1140f23ecaca6c72dd8
VicN <vnikolaeff989@gmail.com>
2022-10-11 12:05:44 +0300

    Project: Cinema Room Manager. Stage 4/5 complete

--- Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -6,29 +6,18 @@ fun main() {
     println("Enter the number of seats in each row:")
     val seats = readln().toInt()
 
-    val allSeats = rows * seats
+    val cinema = Array(rows) { Array(seats) { 'S' } }
 
-    val cinema = MutableList(rows) { MutableList(seats) { "S" } }
-    printCinema(cinema)
-
-    println("Enter a row number:")
-    val row = readln().toInt()
-    println("Enter a seat number in that row:")
-    val seat = readln().toInt()
-
-    val price =
-        if (allSeats <= 60)
-            10
-        else
-            if (row <= rows / 2) 10 else 8
-
-    println("\nTicket price: $$price")
-
-    cinema[row - 1][seat - 1] = "B"
-    printCinema(cinema)
+    var menu: Int
+    do {
+        println("\n1. Show the seats\n2. Buy a ticket\n0. Exit")
+        menu = readln().toInt()
+        if (menu == 1) printCinema(cinema)
+        if (menu == 2) buyTicket(cinema, rows, seats)
+    } while (menu != 0)
 }
 
-fun printCinema(cinema: List<List<Any>>) {
+fun printCinema(cinema: Array<Array<Char>>) {
     print("\nCinema:\n  ")
     repeat(cinema[0].size) { print("${it + 1} ") }
     println()
@@ -40,5 +29,21 @@ fun printCinema(cinema: List<List<Any>>) {
         }
         println()
     }
-    println()
+}
+
+fun buyTicket(cinema: Array<Array<Char>>, rows: Int, seats: Int) {
+    println("\nEnter a row number:")
+    val row = readln().toInt()
+    println("Enter a seat number in that row:")
+    val seat = readln().toInt()
+
+    val price =
+        if (rows * seats <= 60)
+            10
+        else
+            if (row <= rows / 2) 10 else 8
+
+    println("Ticket price: $$price")
+
+    cinema[row - 1][seat - 1] = 'B'
 }
