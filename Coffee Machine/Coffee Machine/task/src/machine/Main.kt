77acec4f940fa89bbf8f78073de162930f05ebe6
viktor <vnikolaeff989@gmail.com>
2022-10-22 16:39:05 +0300

    Project: Coffee Machine. Stage 3/6 complete

--- Coffee Machine/Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/Coffee Machine/task/src/machine/Main.kt
@@ -1,15 +1,31 @@
 package machine
 
 fun main() {
-    println("Write how many cups of coffee you will need:")
-    val water = 200
-    val milk = 50
-    val coffe = 15
+    val useWater = 200
+    val useMilk = 50
+    val useCoffee = 15
 
+    println("Write how many ml of water the coffee machine has:")
+    val water = readln().toInt()
+    var canMakeCups = water / useWater
+
+    println("Write how many ml of milk the coffee machine has:")
+    val milk = readln().toInt()
+    canMakeCups = (milk / useMilk).coerceAtMost(canMakeCups)
+
+    println("Write how many grams of coffee beans the coffee machine has:")
+    val coffee = readln().toInt()
+    canMakeCups = (coffee / useCoffee).coerceAtMost(canMakeCups)
+
+    println("Write how many cups of coffee you will need:")
     val cups = readln().toInt()
 
-    println("For $cups cups of coffee you will need:")
-    println("${cups * water} ml of water")
-    println("${cups * milk} ml of milk")
-    println("${cups * coffe} g of coffee beans")
+    if (cups * useWater <= water && cups * useMilk <= milk && cups * useCoffee <= coffee) {
+        if(canMakeCups > cups)
+            println("Yes, I can make that amount of coffee (and even ${canMakeCups - cups} more than that)")
+        else
+            println("Yes, I can make that amount of coffee")
+    } else {
+        println("No, I can make only $canMakeCups cups of coffee")
+    }
 }
