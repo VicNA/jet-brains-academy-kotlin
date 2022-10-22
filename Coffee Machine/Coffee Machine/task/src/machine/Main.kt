f5121318521a255204017973257e4310ab63ec99
viktor <vnikolaeff989@gmail.com>
2022-10-22 23:42:34 +0300

    Project: Coffee Machine. Stage 4/6. Progress v.1

--- Coffee Machine/Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/Coffee Machine/task/src/machine/Main.kt
@@ -1,31 +1,59 @@
 package machine
 
 fun main() {
-    val useWater = 200
-    val useMilk = 50
-    val useCoffee = 15
+    val coffeeMachine = mutableMapOf(
+        "water" to 400,
+        "milk" to 540,
+        "coffee" to 120,
+        "cups" to 9,
+        "money" to 550
+    )
 
-    println("Write how many ml of water the coffee machine has:")
-    val water = readln().toInt()
-    var canMakeCups = water / useWater
+    printCoffeeMachine(coffeeMachine)
 
-    println("Write how many ml of milk the coffee machine has:")
-    val milk = readln().toInt()
-    canMakeCups = (milk / useMilk).coerceAtMost(canMakeCups)
-
-    println("Write how many grams of coffee beans the coffee machine has:")
-    val coffee = readln().toInt()
-    canMakeCups = (coffee / useCoffee).coerceAtMost(canMakeCups)
-
-    println("Write how many cups of coffee you will need:")
-    val cups = readln().toInt()
-
-    if (cups * useWater <= water && cups * useMilk <= milk && cups * useCoffee <= coffee) {
-        if(canMakeCups > cups)
-            println("Yes, I can make that amount of coffee (and even ${canMakeCups - cups} more than that)")
-        else
-            println("Yes, I can make that amount of coffee")
-    } else {
-        println("No, I can make only $canMakeCups cups of coffee")
+    println("\nWrite action (buy, fill, take):")
+    when (readln()) {
+        "buy" -> buy(coffeeMachine)
+        "fill" -> fill(coffeeMachine)
+        "take" -> TODO()
     }
+
+    printCoffeeMachine(coffeeMachine, 1)
+}
+
+fun printCoffeeMachine(cm: MutableMap<String, Int>, newLine: Int = 0) {
+    repeat(newLine) { print("\n") }
+    println("The coffee machine has:")
+    println("${cm["water"]} ml of water")
+    println("${cm["milk"]} ml of milk")
+    println("${cm["coffee"]} g of coffee beans")
+    println("${cm["cups"]} disposable cups")
+    println("$${cm["money"]} of money")
+}
+
+fun buy(cm: MutableMap<String, Int>) {
+    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+    when (readln()) {
+        "1" -> TODO()
+        "2" -> TODO()
+        "3" -> TODO()
+    }
+}
+
+fun fill(cm: MutableMap<String, Int>) {
+    println("Write how many ml of water you want to add:")
+    cm["water"] = cm.getValue("water") + readln().toInt()
+
+    println("Write how many ml of milk you want to add:")
+    cm["milk"] = cm.getValue("milk") + readln().toInt()
+
+    println("Write how many grams of coffee beans you want to add:")
+    cm["coffee"] = cm.getValue("coffee") + readln().toInt()
+
+    println("Write how many disposable cups you want to add:")
+    cm["cups"] = cm.getValue("cups") + readln().toInt()
+}
+
+fun take(cm: MutableMap<String, Int>) {
+
 }
