3550dc92219116375ad58526c373f4fb72af3ec1
viktor-n <vnikolaeff989@gmail.com>
2022-10-26 14:06:41 +0300

    Project: Coffee Machine. Stage 5/6. Progress v.2

--- Coffee Machine/Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/Coffee Machine/task/src/machine/Main.kt
@@ -7,11 +7,8 @@ var cups = 9
 var money = 550
 
 fun main() {
-
-    remaining()
-
     while (true) {
-        println("Write action (buy, fill, take, remaining, exit):")
+        println("\nWrite action (buy, fill, take, remaining, exit):")
         when (readln()) {
             "buy" -> buy()
             "fill" -> fill()
@@ -35,31 +32,54 @@ fun buy() {
     println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
     when (readln()) {
         "1" -> {
-            if (water > 250) water -= 250
-            coffee -= 16
-            money += 4
+            if (water < 250 || coffee < 16 || cups < 1) {
+                print("Sorry, not enough ")
+                if (water < 250) println("water!")
+                if (coffee < 16) println("coffee!")
+                if (cups < 1) println("cups!")
+            } else {
+                println("I have enough resources, making you a coffee!")
+                water -= 250
+                coffee -= 16
+                cups--
+                money += 4
+            }
         }
-
         "2" -> {
-            water -= 350
-            milk -= 75
-            coffee -= 20
-            money += 7
+            if (water < 350 || milk < 75 || coffee < 20 || cups < 1) {
+                print("Sorry, not enough ")
+                if (water < 350) println("water!")
+                if (milk < 75) println("milk!")
+                if (coffee < 20) println("coffee!")
+                if (cups < 1) println("cups!")
+            } else {
+                println("I have enough resources, making you a coffee!")
+                water -= 350
+                milk -= 75
+                coffee -= 20
+                cups--
+                money += 7
+            }
         }
-
         "3" -> {
-            water -= 200
-            milk -= 100
-            coffee -= 12
-            money += 6
+            if (water < 200 || milk < 100 || coffee < 12 || cups < 1) {
+                print("Sorry, not enough ")
+                if (water < 200) println("water!")
+                if (milk < 100) println("milk!")
+                if (coffee < 12) println("coffee!")
+                if (cups < 1) println("cups!")
+            } else {
+                println("I have enough resources, making you a coffee!")
+                water -= 200
+                milk -= 100
+                coffee -= 12
+                cups--
+                money += 6
+            }
         }
         "back" -> return
     }
-    cups--
-}
 
-fun checkingResources(idx: Int): String {
-    return TODO("перегрузка методов")
 }
 
 fun fill() {
