d5d6fc2cb25b650f6a0ee4519fab3fca1d9cdfdc
VicN <vnikolaeff989@gmail.com>
2022-10-30 11:24:16 +0300

    Project: Coffee Machine. Stage 6/6. Progress v.2

--- Coffee Machine/Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/Coffee Machine/task/src/machine/Main.kt
@@ -1,9 +1,9 @@
 package machine
 
 class CoffeeMachine {
-    var water = 400
-    var milk = 540
-    var coffee = 120
+    var _water = 400
+    var _milk = 540
+    var _coffee = 120
     var cups = 9
     var money = 550
 //    var action = 1
@@ -22,22 +22,22 @@ class CoffeeMachine {
 
     fun remaining() {
         println("\nThe coffee machine has:")
-        println("$water ml of water")
-        println("$milk ml of milk")
-        println("$coffee g of coffee beans")
+        println("$_water ml of water")
+        println("$_milk ml of milk")
+        println("$_coffee g of coffee beans")
         println("$cups disposable cups")
         println("$$money of money")
     }
 
     fun fill() {
         println("\nWrite how many ml of water you want to add:")
-        water += readln().toInt()
+        _water += readln().toInt()
 
         println("Write how many ml of milk you want to add:")
-        milk += readln().toInt()
+        _milk += readln().toInt()
 
         println("Write how many grams of coffee beans you want to add:")
-        coffee += readln().toInt()
+        _coffee += readln().toInt()
 
         println("Write how many disposable cups you want to add:")
         cups += readln().toInt()
@@ -52,50 +52,42 @@ class CoffeeMachine {
         println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
         when (readln()) {
             "1" -> {
-                if (water < 250 || coffee < 16 || cups < 1) {
+                if (_water < 250 || _coffee < 16 || cups < 1) {
                     print("Sorry, not enough ")
-                    if (water < 250) println("water!")
-                    if (coffee < 16) println("coffee!")
+                    if (_water < 250) println("water!")
+                    if (_coffee < 16) println("coffee!")
                     if (cups < 1) println("cups!")
                 } else {
                     println("I have enough resources, making you a coffee!")
-                    water -= 250
-                    coffee -= 16
-                    cups--
+                    makeCoffee(250, 16)
                     money += 4
                 }
             }
 
             "2" -> {
-                if (water < 350 || milk < 75 || coffee < 20 || cups < 1) {
+                if (_water < 350 || _milk < 75 || _coffee < 20 || cups < 1) {
                     print("Sorry, not enough ")
-                    if (water < 350) println("water!")
-                    if (milk < 75) println("milk!")
-                    if (coffee < 20) println("coffee!")
+                    if (_water < 350) println("water!")
+                    if (_milk < 75) println("milk!")
+                    if (_coffee < 20) println("coffee!")
                     if (cups < 1) println("cups!")
                 } else {
                     println("I have enough resources, making you a coffee!")
-                    water -= 350
-                    milk -= 75
-                    coffee -= 20
-                    cups--
+                    makeCoffee(350, 20, 75)
                     money += 7
                 }
             }
 
             "3" -> {
-                if (water < 200 || milk < 100 || coffee < 12 || cups < 1) {
+                if (_water < 200 || _milk < 100 || _coffee < 12 || cups < 1) {
                     print("Sorry, not enough ")
-                    if (water < 200) println("water!")
-                    if (milk < 100) println("milk!")
-                    if (coffee < 12) println("coffee!")
+                    if (_water < 200) println("water!")
+                    if (_milk < 100) println("milk!")
+                    if (_coffee < 12) println("coffee!")
                     if (cups < 1) println("cups!")
                 } else {
                     println("I have enough resources, making you a coffee!")
-                    water -= 200
-                    milk -= 100
-                    coffee -= 12
-                    cups--
+                    makeCoffee(200, 12, 100)
                     money += 6
                 }
             }
@@ -104,8 +96,11 @@ class CoffeeMachine {
         }
     }
 
-    private fun makeCoffee(water: Int, coffee: Int, ) {
-
+    private fun makeCoffee(water: Int, coffee: Int, milk: Int = 0) {
+        _water -= water
+        _coffee -= coffee
+        _milk -= milk
+        cups--
     }
 }
 
