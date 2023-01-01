d01f3a8b3950a4253848fb3511bc814b3372e223
VicN <vnikolaeff989@gmail.com>
2023-01-01 17:30:24 +0300

    Project: Encryption-Decryption. Stage 5/6. Complete

--- Encryption-Decryption/Encryption-Decryption/task/src/Main.kt
+++ Encryption-Decryption/Encryption-Decryption/task/src/Main.kt
@@ -1,25 +1,48 @@
 package encryptdecrypt
 
+import java.io.File
+
 fun main(args: Array<String>) {
     var mode = "enc"
     var key = 0
     var data = ""
+    var input = ""
+    var out = ""
     var arg = ""
 
     for (i in args.indices) {
-        if (args[i] in listOf("-mode", "-key", "-data")) {
+        if (args[i] in listOf("-mode", "-key", "-data", "-in", "-out")) {
             arg = args[i]
         } else {
             when (arg) {
                 "-mode" -> mode = args[i]
                 "-key" -> key = args[i].toInt()
                 "-data" -> data = args[i]
+                "-in" -> input = args[i]
+                "-out" -> out = args[i]
             }
         }
     }
 
+    if (data.isEmpty() && input.isNotEmpty()) {
+        try {
+            data = File(input).readText()
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+        }
+    }
+
     val output = if (mode == "enc") encrypt(data, key) else decrypt(data, key)
-    println(output)
+
+    if (out.isEmpty())
+        println(output)
+    else {
+        try {
+            File(out).writeText(output)
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+        }
+    }
 }
 
 fun decrypt(message: String, key: Int): String {
