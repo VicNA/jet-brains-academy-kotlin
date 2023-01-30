7a52d38fbdf0972c5a25c0a1e902ea98a389fb10
VicN <vnikolaeff989@gmail.com>
2023-01-30 12:02:01 +0300

    Project: Chuck Norris Cipher Encoder. Stage 2/5. Complete

--- Chuck Norris Cipher Encoder/Chuck Norris Cipher Encoder/task/src/chucknorris/Main.kt
+++ Chuck Norris Cipher Encoder/Chuck Norris Cipher Encoder/task/src/chucknorris/Main.kt
@@ -1,3 +1,12 @@
 package chucknorris
 
-fun main() = println("Input string:").let { readln().forEach { print("$it ") } }
+fun main() {
+    println("Input string:")
+    val input = readln()
+
+    println("\nThe result:")
+    for (ch in input) {
+        println("$ch = ${String.format("%7s", Integer.toBinaryString(ch.code))
+            .replace(" ", "0")}")
+    }
+}
