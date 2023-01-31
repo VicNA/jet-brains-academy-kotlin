7b0731da442b947063f208ba519ec8b9f4bc5a61
viktor-n <vnikolaeff989@gmail.com>
2023-01-31 13:30:53 +0300

    Project: Chuck Norris Cipher Encoder. Stage 3/5. Complete

--- Chuck Norris Cipher Encoder/Chuck Norris Cipher Encoder/task/src/chucknorris/Main.kt
+++ Chuck Norris Cipher Encoder/Chuck Norris Cipher Encoder/task/src/chucknorris/Main.kt
@@ -4,9 +4,26 @@ fun main() {
     println("Input string:")
     val input = readln()
 
-    println("\nThe result:")
-    for (ch in input) {
-        println("$ch = ${String.format("%7s", Integer.toBinaryString(ch.code))
-            .replace(" ", "0")}")
+    val sb = StringBuilder()
+    input.forEach { sb.append(Integer.toBinaryString(it.code).padStart(7, '0')) }
+    val array = sb.toString().toCharArray()
+    sb.clear()
+
+    var count = 1
+    for (i in array.indices) {
+        if (i > 0) {
+            if (array[i] == array[i - 1]) {
+                count++
+            } else {
+                if (array[i - 1] == '0') sb.append( " 00 ") else sb.append(" 0 ")
+                sb.append("0".repeat(count))
+                count = 1
+            }
+        }
+        if (i == array.size - 1) {
+            if (array[i] == '0') sb.append( " 00 ") else sb.append(" 0 ")
+            sb.append("0".repeat(count))
+        }
     }
+    println("\nThe result:\n${sb.trim()}")
 }
