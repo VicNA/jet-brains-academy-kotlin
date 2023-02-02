ffb806c2a52d19101bf55bcae723b85bd7bcfcf4
viktor-n <vnikolaeff989@gmail.com>
2023-02-02 09:49:59 +0300

    Project: Chuck Norris Cipher Encoder. Stage 4/5. Complete

--- Chuck Norris Cipher Encoder/Chuck Norris Cipher Encoder/task/src/chucknorris/Main.kt
+++ Chuck Norris Cipher Encoder/Chuck Norris Cipher Encoder/task/src/chucknorris/Main.kt
@@ -3,27 +3,20 @@ package chucknorris
 fun main() {
     println("Input string:")
     val input = readln()
+    val encoded = input.split(" ").chunked(2)
 
     val sb = StringBuilder()
-    input.forEach { sb.append(Integer.toBinaryString(it.code).padStart(7, '0')) }
-    val array = sb.toString().toCharArray()
-    sb.clear()
-
-    var count = 1
-    for (i in array.indices) {
-        if (i > 0) {
-            if (array[i] == array[i - 1]) {
-                count++
-            } else {
-                if (array[i - 1] == '0') sb.append( " 00 ") else sb.append(" 0 ")
-                sb.append("0".repeat(count))
-                count = 1
-            }
-        }
-        if (i == array.size - 1) {
-            if (array[i] == '0') sb.append( " 00 ") else sb.append(" 0 ")
-            sb.append("0".repeat(count))
+    for (enc in encoded) {
+        if (enc.size > 1) {
+            if (enc[0] == "0")
+                sb.append("1".repeat(enc[1].length))
+            else
+                sb.append("0".repeat(enc[1].length))
         }
     }
-    println("\nThe result:\n${sb.trim()}")
+
+    println("\nThe result:")
+    for (str in sb.chunked(7)) {
+        print(Char(str.toInt(2)))
+    }
 }
